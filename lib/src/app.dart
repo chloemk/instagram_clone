1b9e7f8c4dff7c52c0f13d63bd2b88cb0ac08f60
chloemk <minsung.chloe.kim@gmail.com>
2022-07-20 18:37:06 +0900

    feat: add App custom widget for bottomNavigation adn indexed stack

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/src/components/image_data.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Container(),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageData('assets/images/add_friend_icon.jpg'),
+              ),
+            ],
+          ),
+        ),
+        onWillPop: () async {
+          return false;
+        });
   }
 }
