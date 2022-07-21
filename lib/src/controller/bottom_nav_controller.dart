e6aa8b33cb7621c2a7b1b80dc4a4eb312dcff488
chloemk <minsung.chloe.kim@gmail.com>
2022-07-21 11:17:12 +0900

    feat: add PageName enum

--- lib/src/controller/bottom_nav_controller.dart
+++ lib/src/controller/bottom_nav_controller.dart
@@ -1,10 +1,31 @@
 import 'package:get/get.dart';
 
+enum PageName {
+  HOME,
+  SEARCH,
+  UPLOAD,
+  ACTIVITY,
+  MYPAGE,
+}
+
 // Bottom에 있는 페이지 인덱스를 관리하는 클래스
 class BottomNavController extends GetxController {
   RxInt pageIndex = 0.obs;
 
   void changeBottomNav(int value) {
+    var page = PageName.values[value];
+    switch (page) {
+      case PageName.HOME:
+        break;
+      case PageName.SEARCH:
+        break;
+      case PageName.UPLOAD:
+        break;
+      case PageName.ACTIVITY:
+        break;
+      case PageName.MYPAGE:
+        break;
+    }
     pageIndex(value);
   }
 }
