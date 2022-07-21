d82b9fd74091be95873077ea8123a184d5f80f16
chloemk <minsung.chloe.kim@gmail.com>
2022-07-21 13:58:55 +0900

    style: change app exit message popup

--- lib/src/components/message_popup.dart
+++ lib/src/components/message_popup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 // 뒤로가기 눌렀을 때 앱 종료할거냐는 팝업
 class MessagePopup extends StatelessWidget {
@@ -20,21 +21,58 @@ class MessagePopup extends StatelessWidget {
     return Material(
       color: Colors.transparent,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            title!,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 17,
-              color: Colors.black,
-            ),
-          ),
-          Text(
-            message!,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 17,
-              color: Colors.black,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 20,
+              ),
+              width: Get.width * 0.7,
+              child: Column(
+                children: [
+                  Text(
+                    title!,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    message!,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: okCallback,
+                        child: const Text('확인'),
+                      ),
+                      ElevatedButton(
+                        onPressed: cancelCallback,
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.grey,
+                        ),
+                        child: const Text('취소'),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ],
