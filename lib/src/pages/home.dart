b5114130c724e7e8c5bd3ea11d1fbc9a61abef3a
chloemk <minsung.chloe.kim@gmail.com>
2022-07-22 11:12:22 +0900

    feat: implement horizontal scroll story board list

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,42 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/src/components/avatar_widget.dart';
 import 'package:instagram_clone/src/components/image_data.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  Widget _storyBoardList() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        // TODO : generate의 첫번째 인자 length는 스토리의 개수 만큼 생성하면 된다.
+        children: List.generate(
+          100,
+          (index) => AvatarWidget(
+            type: AvatarType.STORYAVATAR,
+            thumbPath: '',
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        titleSpacing: 0,
-        elevation: 0,
-        title: Container(
-          alignment: Alignment.centerLeft,
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: ImageData(
-            IconsPath.logo,
-            width: 300,
+        appBar: AppBar(
+          titleSpacing: 0,
+          elevation: 0,
+          title: Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: ImageData(
+              IconsPath.logo,
+              width: 300,
+            ),
           ),
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: SizedBox(
+                    height: 40,
+                    child: ImageData(
+                      IconsPath.directMessage,
+                      width: 65,
+                    ),
+                  )),
+            ),
+          ],
         ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-                alignment: Alignment.centerRight,
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: SizedBox(
-                  height: 40,
-                  child: ImageData(
-                    IconsPath.directMessage,
-                    width: 65,
-                  ),
-                )),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('123'),
-      ),
-    );
+        body: ListView(
+          children: [
+            _storyBoardList(),
+            // _postList(),
+          ],
+        ));
   }
 }
