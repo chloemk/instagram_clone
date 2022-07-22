bca95ae89230bf5d2f1b0d7296ee8c1b3130d292
chloemk <minsung.chloe.kim@gmail.com>
2022-07-22 13:20:07 +0900

    feat: add myStory Avatar in the story list

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -5,20 +5,42 @@ import 'package:instagram_clone/src/components/image_data.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  Widget _myStory() {
+    return Stack(
+      children: [
+        AvatarWidget(
+          type: AvatarType.MYSTORYAVATAR,
+          thumbPath:
+              'https://backlightblog.com/images/2020/10/blur-photo-background-header.jpg',
+          size: 70,
+        ),
+      ],
+    );
+  }
+
   Widget _storyBoardList() {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        // TODO : generate의 첫번째 인자 length는 스토리의 개수 만큼 생성하면 된다.
-        children: List.generate(
-          100,
-          (index) => AvatarWidget(
-            type: AvatarType.STORYAVATAR,
-            thumbPath:
-                'https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg',
-          ),
-        ),
-      ),
+          // TODO : generate의 첫번째 인자 length는 스토리의 개수 만큼 생성하면 된다.
+          children: [
+            const SizedBox(
+              width: 20,
+            ),
+            _myStory(),
+            const SizedBox(
+              width: 5,
+            ),
+            // ...는 리스트 배열을 나열한다.
+            ...List.generate(
+              100,
+              (index) => AvatarWidget(
+                type: AvatarType.STORYAVATAR,
+                thumbPath:
+                    'https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg',
+              ),
+            ),
+          ]),
     );
   }
 
